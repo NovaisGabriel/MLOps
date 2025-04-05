708b3aed639fe0f54c5d37974bb3ac5fddccc6df
NovaisGabriel <gabrieel.novais@gmail.com>
2025-04-05 10:15:54 -0300

    flask basics

---
+++ flask/README.md
@@ -0,0 +1,5 @@
+# Create an venv in python 
+python -m venv flask_env
+
+# install requirements
+pip install -r requirements.txt
---
+++ flask/app.py
@@ -0,0 +1,12 @@
+from flask import Flask
+'''
+It creates an instance of the Flask class, which will be your WSGI (Web Server Gateway Interface) app.
+'''
+
+app=Flask(__name__)
+
+@app.route()
+
+if __name__=="__main__":
+    app.run()
+
